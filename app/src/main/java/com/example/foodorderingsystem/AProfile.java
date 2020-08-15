2b274c5de5edb492861de7bb95999adb74c52aed
SHADOWRATH <etharuw@gmail.com>
2020-08-15 16:04:53 +0530

    Profiles Created

---
+++ app/src/main/java/com/example/foodorderingsystem/AProfile.java
@@ -0,0 +1,14 @@
+package com.example.foodorderingsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_profile);
+    }
+}
