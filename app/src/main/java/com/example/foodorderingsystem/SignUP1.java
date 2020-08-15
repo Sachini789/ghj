8de02d185c6a24405d7a4c773f543ae6e02fde43
SHADOWRATH <etharuw@gmail.com>
2020-08-15 13:01:53 +0530

    Sign Up prototype

---
+++ app/src/main/java/com/example/foodorderingsystem/SignUP1.java
@@ -0,0 +1,14 @@
+package com.example.foodorderingsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUP1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_u_p1);
+    }
+}
