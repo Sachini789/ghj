9c3d7542b83aeee336a4dde2eeb4055e1b169e3f
SHADOWRATH <etharuw@gmail.com>
2020-09-17 11:02:09 +0530

    new Commit with intents

--- app/src/main/java/com/example/foodorderingsystem/MainActivity.java
+++ app/src/main/java/com/example/foodorderingsystem/MainActivity.java
@@ -2,13 +2,52 @@ package com.example.foodorderingsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText emailID;
+    EditText password;
+    Button btnSignup,btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailID = findViewById(R.id.EmailInput);
+        password = findViewById(R.id.PWInput);
+        btnSignup = findViewById(R.id.SIGNUP);
+        btnLogin = findViewById(R.id.LOGIN);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailID.getText().toString();
+                String pwd = password.getText().toString();
+                if(email.isEmpty())
+                {
+                    emailID.setError("Please Enter your Email");
+                    emailID.requestFocus();
+                }
+                else if(pwd.isEmpty()){
+                    password.setError("Pleae Enter the password");
+                    password.requestFocus();
+                }
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,SignUP1.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/foodorderingsystem/Manage.java
+++ app/src/main/java/com/example/foodorderingsystem/Manage.java
@@ -2,13 +2,40 @@ package com.example.foodorderingsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Manage extends AppCompatActivity {
+
+    ImageButton Cmng,Amng;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage);
+
+        Cmng = findViewById(R.id.CustomerB);
+        Amng = findViewById(R.id.MenuB);
+
+        Cmng.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent x = new Intent(Manage.this,CManage.class);
+                setIntent(x);
+            }
+        });
+
+        Amng.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
